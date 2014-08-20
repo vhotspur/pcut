ddeacacc99cc979b7e2536e63ec06a8e7c08c2a5
Vojtech Horky <vojtech.horky@gmail.com>
2014-08-20 13:34:43 +0200

    Helper macros in a separate file

--- include/pcut/tests.h
+++ include/pcut/tests.h
@@ -36,6 +36,7 @@
 #ifndef PCUT_TESTS_H_GUARD
 #define PCUT_TESTS_H_GUARD
 
+#include <pcut/helper.h>
 #include <pcut/datadef.h>
 
 /*
@@ -79,24 +80,6 @@
  */
 
 /** @cond devel */
-
-/** Join the two arguments on preprocessor level (inner call). */
-#define PCUT_JOIN_IMPL(a, b) a##b
-
-/** Join the two arguments on preprocessor level. */
-#define PCUT_JOIN(a, b) PCUT_JOIN_IMPL(a, b)
-
-/** Quote the parameter (inner call). */
-#define PCUT_QUOTE_IMPL(x) #x
-
-/** Quote the parameter. */
-#define PCUT_QUOTE(x) PCUT_QUOTE_IMPL(x)
-
-/** Get first argument from macro var-args. */
-#define PCUT_VARG_GET_FIRST(x, ...) x
-
-/** Get all but first arguments from macro var-args. */
-#define PCUT_VARG_SKIP_FIRST(x, ...) __VA_ARGS__
 
 /** Produce identifier name for an item with given number.
  *
